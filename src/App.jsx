5a2676caad3bccf01f1d96c5f7468dd5626ca8b5
Patrick Riley <righthandriley32@gmail.com>
2026-01-01 17:49:52 -0500

    changed jobs to columns

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [projects, setProjects] = useState([ { id: 1, name: 'Project 1' }, { id: 2, name: 'Project 2' } ])
-  const [selectedProject, setSelectedProject] = useState(null)
+  const [jobs, setJobs] = useState([ 
+    { id: 1, name: 'Job 1', recipient: 'Recipient 1', message: 'Hi there! We need you to pay up. It has been too long, please take action.' }, 
+    { id: 2, name: 'Job 2', recipient: 'Recipient 2', message: 'Message 2' }, 
+    { id: 3, name: 'Job 3', recipient: 'Recipient 3', message: 'Message 3' },
+    { id: 4, name: 'Job 4', recipient: 'Recipient 4', message: 'Message 4' }
+  ])
+  const [selectedJob, setSelectedJob] = useState(null)
+  const [messagePreview, setMessagePreview] = useState(null)
 
   return (
     <div>
@@ -12,13 +17,14 @@ function App() {
         <h1>Fireservice Follow Up</h1>
       </div>
       <div className='content'>
-        <div className='projects'>
-          {projects.map((project) => (
-            <div key={project.id} onClick={() => setSelectedProject(project)}>
-              {project.name}
+        <div className='jobs'>
+          {jobs.map((job) => (
+            <div key={job.id} className='job'>
+              <h2 onMouseClick={() => { setSelectedJob(job) }}>{job.name}</h2>
             </div>
           ))}
         </div>
+
       </div>
     </div>
   )
